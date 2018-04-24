df9fe2c3f6b15ec5cf4bd98d97c0e5754b11620f
Tocka Ayman <tocka.ayman@outlook.com>
2018-04-24 15:18:38 +0200

    Create goal if it does not exist and update it if it exists

--- src/main/java/org/learning/spring/repository/GoalRepositoryImpl.java
+++ src/main/java/org/learning/spring/repository/GoalRepositoryImpl.java
@@ -17,9 +17,12 @@ public class GoalRepositoryImpl implements GoalRepository {
 
     @Override
     public Goal save(Goal goal) {
-        entityManager.persist(goal);
-        entityManager.flush();
-
+        if (goal.getId() == null) {
+            entityManager.persist(goal);
+            entityManager.flush();
+        } else {
+            goal = entityManager.merge(goal);
+        }
         return goal;
     }
 
