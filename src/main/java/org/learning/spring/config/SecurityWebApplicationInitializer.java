4f2241807bd86d323a5a50d1acab6fe8cbce87c3
Tocka Ayman <tocka.ayman@outlook.com>
2018-04-29 16:27:39 +0200

    Organize imports

--- src/main/java/org/learning/spring/config/SecurityWebApplicationInitializer.java
+++ src/main/java/org/learning/spring/config/SecurityWebApplicationInitializer.java
@@ -1,9 +1,5 @@
 package org.learning.spring.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
